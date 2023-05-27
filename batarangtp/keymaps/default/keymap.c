eefe824573c8f3bd3cc52a5e44833b753052eae4
Protieus <118025702+protieusz@users.noreply.github.com>
2023-05-26 23:04:03 -0700

    Update keymap.c
    Correct encoder vold volu orientation, add directional keys, mouse buttons and positioned the space and backspace better.

--- batarangtp/keymaps/default/keymap.c
+++ batarangtp/keymaps/default/keymap.c
@@ -15,7 +15,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 KC_Q, KC_W, KC_E, KC_R, KC_T,                            KC_Y, KC_U, KC_I, KC_O, KC_P,
 KC_A, KC_S, KC_D, KC_F, KC_G,                            KC_H, KC_J, KC_K, KC_L, 
 KC_Z, KC_X, KC_C, KC_V, KC_B,                            KC_N, KC_M, KC_COMM, KC_DOT, KC_ENT,
-KC_LCTL, KC_LGUI, KC_LALT, KC_MS_BTN1, LT(1,KC_BSPC), KC_MUTE, LT(2,KC_SPC), KC_MS_BTN2, KC_QUOT, KC_SCLN, KC_SLSH
+KC_LCTL, KC_LGUI, LT(3,KC_LALT), KC_MS_BTN1, LT(1,KC_BSPC), KC_MUTE, LT(2,KC_SPC), KC_MS_BTN2, KC_QUOT, KC_SCLN, KC_SLSH
 
 ),
 
@@ -39,20 +39,20 @@ _______, _______, _______, _______, _______, _______, _______, _______, KC_LEFT,
 
 [3] = LAYOUT(
 
-KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,                            KC_F6, KC_F7, KC_F8, KC_F9,KC_PSCR, 
-KC_F12, KC_F13, KC_F14, _______, _______,                     _______, _______, _______, KC_F10,
-_______, _______, _______, _______, _______,                  _______, _______, _______, _______, _______,
-_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
-
+_______, _______, _______, _______, _______,                    KC_F1, KC_F2, KC_F3, KC_F4, KC_PSCR, 
+_______, _______, _______, _______, _______,                  KC_F5, KC_F6, KC_F7, KC_F8,
+_______, _______, _______, _______, _______,                  KC_F9, KC_F10, _______, KC_UP, _______,
+_______, _______, _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT
+  
     )
 };
 
 bool encoder_update_user(uint8_t index, bool clockwise) {
     if (index == 0) {
         if (clockwise) {
-            tap_code(KC_VOLU);
-        } else {
             tap_code(KC_VOLD);
+        } else {
+            tap_code(KC_VOLU);
         }
     }
     return true;
