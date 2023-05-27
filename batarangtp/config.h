02b548d38d0d397c9677847e4fdaef965bfb226e
Protieus <118025702+protieusz@users.noreply.github.com>
2023-05-26 23:00:00 -0700

    Update config.h
    Update attenuate and invert to 90 degree

--- batarangtp/config.h
+++ batarangtp/config.h
@@ -18,8 +18,8 @@
 
 #pragma once
 
-#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET // Activates the double-tap behavior
-#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 200U // Timeout window in ms in which the double tap can occur.
+//#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET // Activates the double-tap behavior
+//#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 200U // Timeout window in ms in which the double tap can occur.
 
 #ifdef COMBO_ENABLE
 #    define COMBO_COUNT 6
@@ -38,14 +38,14 @@
 #define CIRQUE_PINNACLE_DIAMETER_MM 40
 #define CIRQUE_PINNACLE_CURVED_OVERLAY
 #define POINTING_DEVICE_GESTURES_SCROLL_ENABLE // Circular scroll. in this mode enables circular scroll. Touch originating in outer ring can trigger scroll by moving along the perimeter. Near side triggers vertical scroll and far side triggers horizontal scroll.
-#define CIRQUE_PINNACLE_ATTENUATION EXTREG__TRACK_ADCCONFIG__ADC_ATTENUATE_1X
+#define CIRQUE_PINNACLE_ATTENUATION EXTREG__TRACK_ADCCONFIG__ADC_ATTENUATE_3X
 
 
 //Drasha settings
 #define CIRQUE_PINNACLE_TAP_ENABLE //Enable tap to "left click".
 //#define POINTING_DEVICE_GESTURES_CURSOR_GLIDE_ENABLE
 
-#define CIRQUE_PINNACLE_SECONDARY_TAP_ENABLE //Tap in upper right corner (half of the finger needs to be outside of the trackpad) of the trackpad will result in "right click".
+//#define CIRQUE_PINNACLE_SECONDARY_TAP_ENABLE //Tap in upper right corner (half of the finger needs to be outside of the trackpad) of the trackpad will result in "right click".
 
 /* Cirque trackpad. */
 #define SPI_SCK_PIN GP18
