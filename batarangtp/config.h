53d891d56220c8fdebcd9ae0bcdd8ec81a1552e3
Protieus <118025702+protieusz@users.noreply.github.com>
2023-05-29 10:17:19 -0700

    Remove combo count

--- batarangtp/config.h
+++ batarangtp/config.h
@@ -22,7 +22,7 @@
 //#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 200U // Timeout window in ms in which the double tap can occur.
 
 #ifdef COMBO_ENABLE
-#    define COMBO_COUNT 6
+//#    define COMBO_COUNT 6
 #    define COMBO_TERM 200
 #endif
 
